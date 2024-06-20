e735a6e0b1fe80db738cdd281b320d4549992414
trideep.barik <trideep.barik@incture.com>
2024-06-20 13:23:05 +0530

    Tech stacks added

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,23 +1,60 @@
 import React, { useEffect } from "react";
-import Home from "./components/Home/Home";
-import AboutMe from "./components/AboutMe/MeetTheDeveloper";
-import Skills from "./components/Skills/Skills";
-import Footer from "./components/Commons/Footer";
-import Navbar from "./components/Commons/Navbar";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
-
 import SocialBar from "./components/Commons/SocialBar";
 import MeetTheDeveloper from "./components/AboutMe/MeetTheDeveloper";
+import Footer from "./components/Commons/Footer";
+import Marquee from "react-fast-marquee";
+import mainImage from "./images/trideepbarik.png";
+import BubbleUI from "react-bubble-ui";
+import "react-bubble-ui/dist/index.css";
 
 function LandingPage() {
+  const options = {
+    minSize: 120,  // Minimum bubble size
+    gutter: 10,  // Space between bubbles
+    fringeWidth: 100,  // Padding around the edge
+    numCols: 6,
+    yRadius: 100,  // Vertical radius
+    xRadius: 100,  // Horizontal radius
+    cornerRadius: 0,  // Corner radius
+    showGuides: false,  // Show bubble guides
+    compact: true,  // Compact mode
+    gravitation: 5,  // Gravitational pull strength
+  };
+
+  const items = [
+
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png',
+    'https://i0.wp.com/junilearning.com/wp-content/uploads/2020/06/python-programming-language.webp?fit=1920%2C1920&ssl=1',
+    'https://cdn.iconscout.com/icon/free/png-256/free-java-60-1174953.png?f=webp',
+    'https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/redux-icon.png',
+    'https://www.mag-corp.com/wp-content/uploads/2021/08/bootstrap-logo-1.png',
+    'https://mui.com/static/logo.png',
+    'https://www.stickerpress.in/media/products/800x800/83a78dbd45804be5bbd0dd2a82c50fb1.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+    'https://static-00.iconduck.com/assets.00/react-icon-2048x2048-o8k3ymqa.png',
+    'https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png',
+    'https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png',
+    'https://swimburger.net/media/ppnn3pcl/azure.png',
+    'https://www.pngall.com/wp-content/uploads/13/AWS-Logo-PNG-File.png',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI7qVIuRwy4ztyl2tjUFgCMVWntjicY957_g&s',
+    'https://static.vecteezy.com/system/resources/previews/022/227/364/non_2x/openai-chatgpt-logo-icon-free-png.png',
+    'https://www.svgrepo.com/show/303656/php-logo.svg',
+    'https://www.svgrepo.com/show/303251/mysql-logo.svg',
+    'https://pbs.twimg.com/profile_images/1452637606559326217/GFz_P-5e_400x400.png',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Firebase_icon.svg/2048px-Firebase_icon.svg.png',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Socket-io.svg/1024px-Socket-io.svg.png',
+    'https://pbs.twimg.com/profile_images/1235868806079057921/fTL08u_H_400x400.png'
+  ];
+
   return (
-    <>
+    <div className="container">
       <SocialBar />
       <Parallax pages={4} style={{ top: "0", left: "0" }} className="animation">
         <ParallaxLayer offset={0} speed={0.25}>
           <div className="animation_layer parallax" id="background"></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={2.5}>
+        <ParallaxLayer offset={0} speed={2}>
           <div className="animation_layer parallax landing-text-container">
             <h1 className="display-2 landing-text">A DEVELOPER'S ODYSSEY</h1>
           </div>
@@ -25,10 +62,10 @@ function LandingPage() {
         <ParallaxLayer offset={0} speed={0.3}>
           <div className="animation_layer parallax" id="mountain6"></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={-0.1}>
+        <ParallaxLayer offset={0} speed={-0.2}>
           <div className="animation_layer parallax" id="mountain5"></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.3}>
+        <ParallaxLayer offset={0} speed={0}>
           <div className="animation_layer parallax" id="mountain4"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={1} speed={0}>
@@ -40,11 +77,9 @@ function LandingPage() {
             style={{ minHeight: "100vh" }}
           >
             <div className="col-md-5">
-              <p className="display-3">What's in the Tech Voyager's Toolkit?</p>
-            </div>
-            <div className="col-md-5">
+              <p className="display-2">What's in the Tech Voyager's Toolkit?</p>
               <p
-                className="h6"
+                className="developer-text"
                 style={{ width: "80%", color: "var(--secondary-color)" }}
               >
                 In my digital toolkit, I wield a powerful set of skills tailored
@@ -57,6 +92,15 @@ function LandingPage() {
                 journey.
               </p>
             </div>
+            <div className="col-md-5">
+              <BubbleUI options={options} className="myBubbleUI">
+                {items.map((item, index) => (
+                  <div key={index} className="child">
+                    <img src={item} alt={`Bubble ${index}`} className="bubble-image" />
+                  </div>
+                ))}
+              </BubbleUI>
+            </div>
           </div>
         </ParallaxLayer>
         <ParallaxLayer
@@ -67,7 +111,7 @@ function LandingPage() {
           <Footer />
         </ParallaxLayer>
       </Parallax>
-    </>
+    </div>
   );
 }
 
--- src/components/AboutMe/MeetTheDeveloper.jsx
+++ src/components/AboutMe/MeetTheDeveloper.jsx
@@ -1,10 +1,27 @@
-import Marquee from "react-fast-marquee";
-import mainImage from "../../images/trideepbarik.png";
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+import mainImage from '../../images/trideepbarik.png';
+import Typing from 'react-typing-effect';
 
 const MeetTheDeveloper = () => {
+  const displayTextRenderer = (text, i) => {
+    return (
+      <p key={i} className="developer-text">
+        {text.split('').map((char, index) => {
+          const key = `${char}-${index}`;
+          return (
+            <span key={key} style={{ color: 'var(--secondary-color)' }}>
+              {char}
+            </span>
+          );
+        })}
+      </p>
+    );
+  };
+
   return (
     <div className="row main-section d-flex justify-content-center align-items-center">
-      <div className="col-md-12" style={{ marginBottom: "-30vh", zIndex: "0" }}>
+      <div className="col-md-12" style={{ marginBottom: '-30vh', zIndex: '0' }}>
         <Marquee
           autoFill={true}
           pauseOnHover={true}
@@ -13,33 +30,21 @@ const MeetTheDeveloper = () => {
           gradientColor="#163034"
         >
           <p className="display-2">&nbsp;&nbsp;&nbsp;</p>
-          <p className="display-2">Meet The Developer</p>
+          <p className="display-2 marquee-text">Meet The Developer</p>
           <p className="display-2">&nbsp;&nbsp;&nbsp;</p>
         </Marquee>
       </div>
-      <div className="col-md-5" style={{ zIndex: 1 }}>
+      <div className="col-md-4" style={{ zIndex: 1 }}>
         <img src={mainImage} alt="Profile" className="img-fluid" />
       </div>
-      <div className="col-md-5">
-        <p
-          className="h6"
-          style={{ width: "80%", color: "var(--secondary-color)" }}
-        >
-          I'm{" "}
-          <span style={{ color: "var(--primary-color)" }}>Trideep Barik</span>
-          , a tech enthusiast driven by the thrill of creating digital marvels.
-          Embarking on a journey across the ever-evolving tech landscape, I've
-          passionately transformed concepts into enchanting tech solutions. Join
-          me as we navigate the dynamic realms where lines of code spark
-          innovation.
-          <br />
-          <br />
-          In this odyssey, I find joy in embracing the unknown, constantly
-          venturing beyond my comfort zone. My love for learning propels me
-          towards new horizons, unraveling the mysteries of technology with
-          every step. Together, let's delve into the boundless possibilities
-          that the world of tech has to offer.
-        </p>
+      <div className="col-md-1"></div>
+      <div className="col-md-4">
+        <Typing displayTextRenderer={displayTextRenderer} speed={5} cursor={" "} eraseDelay={99999999} className='developer-text' text={[
+          "I'm Trideep Barik, a tech enthusiast driven by the thrill of creating digital marvels. Embarking on a journey across the ever-evolving tech landscape, I've passionately transformed concepts into enchanting tech solutions."]}
+        />
+        <Typing displayTextRenderer={displayTextRenderer} speed={5} cursor={" "} typingDelay={6000} eraseDelay={99999999} className='developer-text' text={[
+          "Join me as we navigate the dynamic realms where lines of code spark innovation. In this odyssey, I find joy in embracing the unknown, constantly venturing beyond my comfort zone. My love for learning propels me towards new horizons, unraveling the mysteries of technology with every step. Together, let's delve into the boundless possibilities that the world of tech has to offer."]}
+        />
       </div>
     </div>
   );
